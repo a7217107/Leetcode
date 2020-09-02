1484df6c09a636d21dd32b8b01a97f26e934c1fd
a7217107 <a913377355@qq.com>
2020-09-02 13:57:18 +0800

    日常打卡

---
+++ xbj/src/Leetcode77.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode77 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 1; i < n; i++) {
+
+        }
+        return res;
+    }
+}
